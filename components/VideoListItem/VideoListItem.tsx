fce87f4c69fde7cc2c679ead1dcddf1a4711a1df
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-05-30 11:38:45 +0800

    qss

--- components/VideoListItem/VideoListItem.tsx
+++ components/VideoListItem/VideoListItem.tsx
@@ -6,16 +6,20 @@ import VideoItem from "../VideoItem";
 const videoData = [
   {
     id: "1",
-    thumbnailUrl: "https://example.com/thumbnail1.jpg",
-    channelAvatarUrl: "https://example.com/channel1.jpg",
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/gsOE7pGZRec/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAbFyQbJcwE6WeEFUGip3TWi67ORA",
+    channelAvatarUrl:
+      "https://yt3.ggpht.com/SBra9X0UPsNzyDjGcvN4iw2pb5Qku_3qFd1figMt48J7rUdU8mJYHPHAx0t6d0E6wLcUU__nZjg=s68-c-k-c0x00ffffff-no-rj",
     title: "Video 1",
     channelName: "Channel 1",
     views: 1000,
   },
   {
     id: "2",
-    thumbnailUrl: "https://example.com/thumbnail2.jpg",
-    channelAvatarUrl: "https://example.com/channel2.jpg",
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/TTjgLB0v4EA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA6gCjm4PgBCopePFbZGwHp7flwKA",
+    channelAvatarUrl:
+      "https://yt3.ggpht.com/SBra9X0UPsNzyDjGcvN4iw2pb5Qku_3qFd1figMt48J7rUdU8mJYHPHAx0t6d0E6wLcUU__nZjg=s68-c-k-c0x00ffffff-no-rj",
     title: "Video 2",
     channelName: "Channel 2",
     views: 500,
