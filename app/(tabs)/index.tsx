21e5bc924054197a47bcc9921f6f55f3cea427ef
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-05-31 20:55:32 +0800

    jkkjj

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,10 @@
-import { StyleSheet } from "react-native";
 import { Text, View } from "../../components/Themed";
 import VideoListItem from "../../components/VideoListItem";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1">
       <VideoListItem />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
--- components/VideoItem/VideoItem.tsx
+++ components/VideoItem/VideoItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { View, Image, Text } from "react-native";
 import Video from "../Video";
 
 interface VideoItemProps {
@@ -8,59 +8,18 @@ interface VideoItemProps {
 
 const VideoItem: React.FC<VideoItemProps> = ({ video }) => {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: video.thumbnailUrl }} style={styles.thumbnail} />
-      <View style={styles.detailsContainer}>
-        <Image
-          source={{ uri: video.channelAvatarUrl }}
-          style={styles.channelAvatar}
-        />
-        <View style={styles.textContainer}>
-          <Text style={styles.title}>{video.title}</Text>
-          <Text style={styles.channelName}>{video.channelName}</Text>
-          <Text style={styles.views}>{video.views} views</Text>
+    <View className="flex">
+      <Image source={{ uri: video.thumbnailUrl }} className="h-48 w-screen" />
+      <View>
+        <Image source={{ uri: video.channelAvatarUrl }} />
+        <View className="flex items-start float-right">
+          <Text>{video.title}</Text>
+          <Text>{video.channelName}</Text>
+          <Text>{video.views} views</Text>
         </View>
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    marginBottom: 10,
-  },
-  thumbnail: {
-    width: "100%",
-    height: 200,
-  },
-  detailsContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingVertical: 10,
-  },
-  channelAvatar: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 10,
-  },
-  textContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 5,
-  },
-  channelName: {
-    fontSize: 16,
-    color: "#555",
-    marginBottom: 5,
-  },
-  views: {
-    fontSize: 14,
-    color: "#777",
-  },
-});
 
 export default VideoItem;
