8673850ca259713750685143d0ce57fb961c9d13
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-05-31 21:48:28 +0800

    bnbbj

--- components/VideoItem/VideoItem.tsx
+++ components/VideoItem/VideoItem.tsx
@@ -1,22 +1,39 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, Dimensions } from "react-native";
 import Video from "../Video";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 interface VideoItemProps {
   video: Video;
 }
 
 const VideoItem: React.FC<VideoItemProps> = ({ video }) => {
+  const screenWidth = Dimensions.get("window").width;
+
   return (
     <View className="flex">
-      <Image source={{ uri: video.thumbnailUrl }} className="h-48 w-screen" />
-      <View>
-        <Image source={{ uri: video.channelAvatarUrl }} />
-        <View className="flex items-start float-right">
-          <Text>{video.title}</Text>
-          <Text>{video.channelName}</Text>
-          <Text>{video.views} views</Text>
+      <Image source={{ uri: video.thumbnailUrl }} className="h-48 w-full" />
+      <View className="pt-2 flex flex-row items-center">
+        <Image
+          source={{ uri: video.channelAvatarUrl }}
+          className="h-10 w-10 rounded-full"
+        />
+        <View className="flex ml-2">
+          <Text className="text-base font-bold line-clamp-1">
+            {video.title}
+          </Text>
+          <View className="flex flex-row items-center">
+            <Text className="text-sm text-gray-500">{video.channelName}</Text>
+            <Text className="text-sm text-gray-500 mx-1">•</Text>
+            <Text className="text-sm text-gray-500">{video.views} views</Text>
+          </View>
         </View>
+        <MaterialCommunityIcons
+          name="dots-vertical"
+          size={25}
+          color="gray"
+          className="ml-auto"
+        />
       </View>
     </View>
   );
--- components/VideoListItem/VideoListItem.tsx
+++ components/VideoListItem/VideoListItem.tsx
@@ -10,8 +10,8 @@ const videoData = [
       "https://i.ytimg.com/vi/gsOE7pGZRec/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAbFyQbJcwE6WeEFUGip3TWi67ORA",
     channelAvatarUrl:
       "https://yt3.ggpht.com/SBra9X0UPsNzyDjGcvN4iw2pb5Qku_3qFd1figMt48J7rUdU8mJYHPHAx0t6d0E6wLcUU__nZjg=s68-c-k-c0x00ffffff-no-rj",
-    title: "Video 1",
-    channelName: "Channel 1",
+    title: "Next.js Full Course for Beginners | Nextjs 13 Tutorial | 7 Hours",
+    channelName: "Dave Gray",
     views: 1000,
   },
   {
@@ -20,9 +20,20 @@ const videoData = [
       "https://i.ytimg.com/vi/TTjgLB0v4EA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA6gCjm4PgBCopePFbZGwHp7flwKA",
     channelAvatarUrl:
       "https://yt3.ggpht.com/SBra9X0UPsNzyDjGcvN4iw2pb5Qku_3qFd1figMt48J7rUdU8mJYHPHAx0t6d0E6wLcUU__nZjg=s68-c-k-c0x00ffffff-no-rj",
-    title: "Video 2",
-    channelName: "Channel 2",
+    title:
+      "Neufc/OFC/BFC Transfer Updates | Neufc Assistant Coach? | El khayati",
+    channelName: "Beyond Fireship",
     views: 500,
+  },
+  {
+    id: "3",
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/TTjgLB0v4EA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA6gCjm4PgBCopePFbZGwHp7flwKA",
+    channelAvatarUrl:
+      "https://yt3.ggpht.com/SBra9X0UPsNzyDjGcvN4iw2pb5Qku_3qFd1figMt48J7rUdU8mJYHPHAx0t6d0E6wLcUU__nZjg=s68-c-k-c0x00ffffff-no-rj",
+    title: "I found the perfect component library",
+    channelName: "Fireship",
+    views: 1000,
   },
 ];
 
