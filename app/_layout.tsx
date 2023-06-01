b8ff654b309a269007d5f2665717011a1a71682e
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-01 22:58:01 +0800

    qe

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
   DarkTheme,
@@ -7,7 +8,8 @@ import {
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import { useColorScheme } from "react-native";
+import { View, useColorScheme, Text, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -17,6 +19,39 @@ export {
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: "(tabs)",
+};
+
+const HeaderComponent = () => {
+  const image = "https://download.logo.wine/logo/YouTube/YouTube-Logo.wine.png";
+  const avatar =
+    "https://yt3.ggpht.com/SBra9X0UPsNzyDjGcvN4iw2pb5Qku_3qFd1figMt48J7rUdU8mJYHPHAx0t6d0E6wLcUU__nZjg=s68-c-k-c0x00ffffff-no-rj";
+
+  return (
+    <SafeAreaView className="flex-row justify-between mx-4">
+      <View className="flex-row items-center space-x-1">
+        {/* <Image source={image} style={{ height: 28, width: 40 }} /> */}
+        <FontAwesome name="youtube-play" size={28} color="red" />
+        <Text className="text-black font-semibold tracking-tighter text-lg">
+          YouTube
+        </Text>
+      </View>
+      <View className="flex-row items-center space-x-3">
+        <MaterialCommunityIcons
+          name="bell"
+          size={24}
+          color="black"
+          style={{ marginRight: 8 }}
+        />
+        <FontAwesome
+          name="search"
+          size={24}
+          color="black"
+          style={{ marginRight: 8 }}
+        />
+        <Image source={{ uri: avatar }} className="h-7 w-7 rounded-full" />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default function RootLayout() {
@@ -46,7 +81,12 @@ function RootLayoutNav() {
     <>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" />
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              header: HeaderComponent,
+            }}
+          />
           <Stack.Screen name="modal" options={{ presentation: "modal" }} />
         </Stack>
       </ThemeProvider>
