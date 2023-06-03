419331175cb70816491a5ca89eb0652875facd89
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-03 21:53:52 +0800

    wdwd

--- components/VideoItem/VideoItem.tsx
+++ components/VideoItem/VideoItem.tsx
@@ -40,73 +40,74 @@ const VideoItem: React.FC<VideoItemProps> = ({ video }) => {
           source={{ uri: video.thumbnailUrl }}
           style={{ width: screenWidth, height: screenWidth * 0.56 }} // Assuming 16:9 aspect ratio for the video thumbnail
         />
-      </Pressable>
-      <View style={{ padding: 4 }}>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <TouchableOpacity onPress={handleChannelAvatarPress}>
-            <Image
-              source={{ uri: video.channelAvatarUrl }}
-              style={{ width: 32, height: 32, borderRadius: 16 }}
-            />
-          </TouchableOpacity>
-          <Text
-            numberOfLines={2}
-            ellipsizeMode="tail"
-            style={{
-              flex: 1,
-              marginLeft: 8,
-              fontSize: 14,
-              fontWeight: "bold",
-            }}
-          >
-            {video.title}
-          </Text>
-          <TouchableOpacity onPress={handleMenuPress}>
-            <MaterialCommunityIcons
-              name={showMenu ? "dots-vertical" : "dots-horizontal"}
-              size={24}
-              color="gray"
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={{ flexDirection: "row", marginTop: 4 }}>
-          <Text style={{ fontSize: 12, color: "gray" }}>
-            {video.channelName}
-          </Text>
-          <Text
-            style={{
-              marginLeft: 4,
-              marginRight: 4,
-              fontSize: 12,
-              color: "gray",
-            }}
-          >
-            •
-          </Text>
-          <Text style={{ fontSize: 12, color: "gray" }}>
-            {video.views} views
-          </Text>
-          <Text
-            style={{
-              marginLeft: 4,
-              marginRight: 4,
-              fontSize: 12,
-              color: "gray",
-            }}
-          >
-            •
-          </Text>
-          <Text style={{ fontSize: 12, color: "gray" }}>
-            {video.uploadedAt}
-          </Text>
-        </View>
-        {showMenu && (
-          <View style={{ marginTop: 8 }}>
-            {/* Add your menu content here */}
-            <Text>Menu Content</Text>
+
+        <View style={{ padding: 4 }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <TouchableOpacity onPress={handleChannelAvatarPress}>
+              <Image
+                source={{ uri: video.channelAvatarUrl }}
+                style={{ width: 32, height: 32, borderRadius: 16 }}
+              />
+            </TouchableOpacity>
+            <Text
+              numberOfLines={2}
+              ellipsizeMode="tail"
+              style={{
+                flex: 1,
+                marginLeft: 8,
+                fontSize: 14,
+                fontWeight: "bold",
+              }}
+            >
+              {video.title}
+            </Text>
+            <TouchableOpacity onPress={handleMenuPress}>
+              <MaterialCommunityIcons
+                name={showMenu ? "dots-vertical" : "dots-horizontal"}
+                size={24}
+                color="gray"
+              />
+            </TouchableOpacity>
           </View>
-        )}
-      </View>
+          <View style={{ flexDirection: "row", marginTop: 4 }}>
+            <Text style={{ fontSize: 12, color: "gray" }}>
+              {video.channelName}
+            </Text>
+            <Text
+              style={{
+                marginLeft: 4,
+                marginRight: 4,
+                fontSize: 12,
+                color: "gray",
+              }}
+            >
+              •
+            </Text>
+            <Text style={{ fontSize: 12, color: "gray" }}>
+              {video.views} views
+            </Text>
+            <Text
+              style={{
+                marginLeft: 4,
+                marginRight: 4,
+                fontSize: 12,
+                color: "gray",
+              }}
+            >
+              •
+            </Text>
+            <Text style={{ fontSize: 12, color: "gray" }}>
+              {video.uploadedAt}
+            </Text>
+          </View>
+          {showMenu && (
+            <View style={{ marginTop: 8 }}>
+              {/* Add your menu content here */}
+              <Text>Menu Content</Text>
+            </View>
+          )}
+        </View>
+      </Pressable>
     </View>
   );
 };
