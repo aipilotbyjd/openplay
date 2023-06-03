45acd81ea2fde1a3bfb3a356d08b1499c05cb1ac
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-06-03 21:40:05 +0800

    dwd

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,9 @@ export default function TabOneScreen() {
       <ScrollView className="flex-1 mt-0" showsVerticalScrollIndicator={false}>
         {/* categories */}
         <HeaderCategory />
-        <VideoListItem />
+        <ScrollView>
+          <VideoListItem />
+        </ScrollView>
       </ScrollView>
     </View>
   );
